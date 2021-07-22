9e6532e659b01f8e49bec78f590f523931ecff99
Bluenix <38372706+Bluenix2@users.noreply.github.com>
2021-07-22 11:54:05 +0000

    Only allow one authentication method at a time
    After discussion in the Discord server it was agreed that multiple ways to authorize would unfairly give experienced users an advantage. This way,we still have the element of different ways to authorize, but it is now an even playing field.

--- pixels/auth/__init__.py
+++ pixels/auth/__init__.py
@@ -1,28 +1,5 @@
-from fastapi import Request
-from fastapi.security.utils import get_authorization_scheme_param
-
 from .basic import BasicAuth
 from .jwt import JWTBearer
 
-
-class UserAuth:
-    """Dependency for routes to enforce any kind of supported authentication."""
-
-    def __init__(self, is_mod_endpoint: bool = False):
-        self.is_mod_endpoint = is_mod_endpoint
-
-        self.schemes = {
-          'Basic': BasicAuth(),
-          'Bearer': self.jwt = JWTBearer(),
-        }
-
-    async def __call__(self, request: Request):
-        authorization = request.headers.get('Authorization')
-        scheme, param = get_authorization_scheme_param(authorization)
-
-        try:
-            res = await self.schemes[scheme](request)
-        except KeyError:
-            raise HTTPException(status_code=401, detail="Unsupported authentication method.")
-
-        return res
+# The current authorization method
+UserAuth = JWTBearer
